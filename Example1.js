a74bcdef867a2827d0b8aa91f45d5e9903b0c3c2
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-25 12:38:16 +0500

    Create Example1.js

---
+++ Example1.js
@@ -0,0 +1,16 @@
+
+
+function times(t){
+    for(var i = 0; i<11; i++){
+        t();
+    }
+    
+}
+
+
+function sas(){
+    let a = Math.floor(Math.random() * 20) + 1;
+    document.write(a)
+}
+
+times(sas)
